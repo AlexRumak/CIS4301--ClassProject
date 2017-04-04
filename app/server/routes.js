7691ff6d4dfb06d7e9dffcf19298cac6f4636ed4
Hugh <hugh.j.wu@gmail.com>
2017-04-04 19:15:50 -0400

    Added some new web pages

--- app/server/routes.js
+++ app/server/routes.js
@@ -1,19 +1,21 @@
 /*
 This file contains the paths we direct users when they navigate the site
-'/' Refers to the home page
+'/' Refers to the home page (in our case localhost:port)
 */
 
-module.exports = function(app, passport){
-    // Home page
-    app.get('/', function(req, res) {
-        res.render('./app/public/views/index.html');
-    });
+var express = require('express');
+var router = express.Router();
 
-    // Login page
-    app.get('/login', function(req, res){
-        res.render('./app/public/views/login.html', {message: req.flash('loginMessage') });
-    });
+router.get('/', function(req, res){
+    res.send('Home Page');
+});
 
-    
+router.get('/login', function (req, res){
+    res.render('login.ejs');
+});
 
-}
+router.get('/action', function(req, res){
+    res.render('action.ejs');
+});
+
+module.exports = router;
