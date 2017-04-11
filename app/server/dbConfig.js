b8c2a6b02dce08dbebc80f9a163a9325d4ffebf6
Hugh <hugh.j.wu@gmail.com>
2017-04-11 15:07:35 -0400

    Added comments and updated views files

--- app/server/dbConfig.js
+++ app/server/dbConfig.js
@@ -3,7 +3,7 @@ This file contains the credentials needed for connecting to the Oracle server
 */
 
 module.exports = {
-    user: "", // Put in your Oracle user name here
-    password: "", // Password here
+    user: "hjwu", // Put in your Oracle user name here
+    password: "wughOracle3!", // Password here
     connectString: "oracle.cise.ufl.edu:1521/orcl" // The specific CISE Oracle DB instance
 };
