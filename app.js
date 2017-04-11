249284bb6a8bbc8737ad3786d61aa80236f7d731
Hugh <hugh.j.wu@gmail.com>
2017-04-11 15:07:35 -0400

    Added comments and updated views files

--- app.js
+++ app.js
@@ -6,23 +6,27 @@ var express = require('express');
 var app = express();
 var port = 8000;
 var bodyParser = require('body-parser');
-var oracledb = require('oracledb');
-    oracledb.autoCommit = true;
-var dbConfig = require(__dirname + '/app/server/dbConfig.js');
 var bcrypt = require('bcrypt');
 var flash = require('connect-flash');
 var session = require('express-session');
-// var cookieParser = require('cookie-parser');
+var oracledb = require('oracledb');
+    oracledb.autoCommit = true;
+var oracledbStore = require('express-oracle-session')(session);
+var dbConfig = require(__dirname + '/app/server/dbConfig.js');
 
-var queryRunner = function(queryString, callback){
-    oracledb.getConnection(
-    {
+var credentials = {
         user:           dbConfig.user,
         password:       dbConfig.password,
         connectString:  dbConfig.connectString
-    }, 
+}
+
+var sessionStore = new oracledbStore(credentials);
+
+// Funtcion used to run the queries against the OracleDB
+var queryRunner = function(queryString, callback){
+    oracledb.getConnection(credentials, 
     function(err, connection)
-    {
+        {
         if(err){
             console.log(err.message);
             connection.close();
@@ -44,19 +48,18 @@ var queryRunner = function(queryString, callback){
 
 // Configure the server
 // ***************************************************
-app.set('view engine', 'ejs');
-app.set('views', __dirname + '/app/public/views');
-app.use(express.static(__dirname + '/app/public'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(session({
-    secret: 'secret-pepe-found',
+app.set('view engine', 'ejs'); // Renders the pages that are displayed to users
+app.set('views', __dirname + '/app/public/views'); // Set the views directory
+app.use(express.static(__dirname + '/app/public')); // Set the directory to serve static files (CSS, images, etc.)
+app.use(bodyParser.json()); // Used to parse JSON objects passed
+app.use(bodyParser.urlencoded({extended: true})); // Used to parse the form data
+app.use(session({ // Creates lasting sessions
+    secret: 'secret-pepe-found', 
     saveUninitialized: false,
-    resave: false
+    resave: false,
+    store: sessionStore
 }));
-app.use(flash());
-
-
+app.use(flash()); // Used to display the warning messages when login/signup fails
 
 // Routes
 // Defines the different web pages users are directed to
@@ -64,32 +67,40 @@ app.use(flash());
 // '/login' Refers to localhost:port/login
 // ***************************************************
 
-// Default home page
-// Leads them to login or signup 
+// Home page - leads them to login or signup 
 app.get('/', function(req, res){
-    res.render('index.ejs');
+    if(req.session.user){
+        res.redirect('/action');
+    } else{
+        res.render('index.ejs', {user: req.session.user});
+    }
 });
 
 // Page to login 
 app.get('/login', function(req, res){
     var invalidUserMessage = req.flash('message')[0];
-    res.render('login.ejs', {message: invalidUserMessage});
+    res.render('login.ejs', {user: req.session.user, message: invalidUserMessage});
 });
 
+// Called when a form gets submitted in the login  page
 app.post('/login', function(req, res){
     var user = req.body.user;
     var password = req.body.password;
 
-     queryRunner(`SELECT username, password FROM Testuser WHERE username = '${user}'`, function(err, result){
+    // Checks if the user exists
+    // If the user doesn't exist, reload the login page
+    queryRunner(`SELECT username, password FROM Testuser WHERE username = '${user}'`, function(err, result){
         if(result.rows.length === 0 ){
             req.flash('message', 'Unable to find username or password');
             res.redirect('/login');
         } else{
+            // If user exists, compare their hashed password with the password stored in the DB
             var salt = bcrypt.genSaltSync(10);
             var hashedPasswordFromDatabase = result.rows[0][1];
             if(bcrypt.compareSync(password, hashedPasswordFromDatabase)){
+                // Start the session and set the user variable to be the username
                 req.session.user = user;
-                res.redirect('/test');
+                res.redirect('/action');
             } else {
             req.flash('message', 'Unable to find username or password');
             res.redirect('/login');
@@ -102,10 +113,10 @@ app.post('/login', function(req, res){
 // Page to signup
 app.get('/signup', function(req, res){
     var noUserExistsMessage = req.flash('message')[0];
-    res.render('signup.ejs', {message: noUserExistsMessage});
+    res.render('signup.ejs', {user: req.session.user, message: noUserExistsMessage});
 });
 
-
+// Called when the user sumits a form on the signup page
 app.post('/signup', function(req, res){
     // Check if the username is taken
     var user = req.body.user;
@@ -114,7 +125,7 @@ app.post('/signup', function(req, res){
             req.flash('message', 'The username already exists');
             res.redirect('/signup');
         } else{
-                    
+            // Hash the password before storing it in the database
             var password = req.body.password;
             var salt = bcrypt.genSaltSync(10);
             var hashedPassword = bcrypt.hashSync(password, salt);
@@ -129,6 +140,10 @@ app.post('/signup', function(req, res){
         }
     });
 });
+
+app.get('/action', function(req, res){
+    res.render('action.ejs', {user: req.session.user});
+})
 
 // Testing queries
 app.get('/test', function(req, res){
@@ -148,6 +163,12 @@ app.post('/test', function(req, res){
     });
 });
 
+// Testing if session works
+app.get('/session', function(req, res){
+    res.send(req.session);
+});
+
+// Start the server
 app.listen(port, function(){
     console.log('Listening on port ' + port);
 });
