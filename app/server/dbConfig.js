140521a84bc948d6664876ade2758cccaa4c8bf8
Hugh <hugh.j.wu@gmail.com>
2017-04-12 17:35:09 -0400

    Oops

--- app/server/dbConfig.js
+++
@@ -1,9 +0,0 @@
-/* 
-This file contains the credentials needed for connecting to the Oracle server
-*/
-
-module.exports = {
-    user: "hjwu", // Put in your Oracle user name here
-    password: "wughOracle3!", // Password here
-    connectString: "oracle.cise.ufl.edu:1521/orcl" // The specific CISE Oracle DB instance
-};
