5c7324517fa08d5991e03840e1a217d4038efe7f
Hugh <hugh.j.wu@gmail.com>
2017-04-13 12:47:44 -0400

    Whole buncha stuff

--- app/app.js
+++ app/app.js
@@ -49,7 +49,7 @@ app.post('/login', function(req, res){
 
     // Checks if the user exists
     // If the user doesn't exist, reload the login page
-    connection.queryRunner(`SELECT username, password FROM websiteUsers WHERE username = '${user}'`, function(err, result){
+    connection.queryRunner(`SELECT username, password FROM testUser WHERE username = '${user}'`, function(err, result){
         if(result.rows.length === 0 ){
             req.flash('message', 'Unable to find username or password');
             res.redirect('/login');
@@ -80,7 +80,7 @@ app.get('/signup', function(req, res){
 app.post('/signup', function(req, res){
     // Check if the username is taken
     var user = req.body.user;
-    connection.queryRunner(`SELECT username FROM websiteUsers WHERE username = '${user}'`, function(err, result){
+    connection.queryRunner(`SELECT username FROM testUser WHERE username = '${user}'`, function(err, result){
         if(result.rows.length > 0){
             req.flash('message', 'The username already exists');
             res.redirect('/signup');
@@ -90,7 +90,7 @@ app.post('/signup', function(req, res){
             var salt = bcrypt.genSaltSync(10);
             var hashedPassword = bcrypt.hashSync(password, salt);
 
-            var query = `INSERT INTO websiteUsers (username, password) values ('${user}', '${hashedPassword}')`;
+            var query = `INSERT INTO testUser (username, password) values ('${user}', '${hashedPassword}')`;
             connection.queryRunner(query, function(err, result){
                 if(err){
                     return err;
@@ -102,7 +102,6 @@ app.post('/signup', function(req, res){
 });
 
 app.post('/logout', function(req, res){
-    console.log('aotweiofoe');
     req.session.destroy();
     res.redirect('/');
 });
@@ -119,11 +118,6 @@ app.get('/', function(req, res){
 // Giant search form page
 app.get('/search', function(req, res){
    res.render('search.ejs', {user: req.session.user});
-});
-
-// Render the results on a search submission
-app.post('/search', function(req, res){
-
 });
 
 app.get('/action', function(req, res){
@@ -155,6 +149,8 @@ app.get('/search', function(req, res){
 
 // Render the results on a search submission
 app.get('/results', function(req, res){
+    // Function below breaks the URL up given
+    // a URI such as /results?params='paramValue'&moreParams
     function getParameterByName(name, url) {
         if (!url) {
             url = req.url;
@@ -172,12 +168,11 @@ app.get('/results', function(req, res){
     }
 
     var firstName = getParameterByName('first');
-    firstName = "";
     var lastName = getParameterByName('last');
 
-    var queryString = `SELECT * FROM Inmate WHERE fistname like '%${firstName}%'`; 
-    //AND lastname like '${lastName}'`;
-    queryRunner(queryString, function(err, results){
+    var queryString = `SELECT * FROM Inmate where firstName like '%${firstName}%' OR lastName like '%${lastName}%'`;
+
+    connection.queryRunner(queryString, function(err, results){
         if(err){
             return err;
         }
@@ -185,9 +180,72 @@ app.get('/results', function(req, res){
     });
 });
 
-// Testing if session works
-app.get('/session', function(req, res){
-    res.send(req.session);
+// Render the results on a residence search submission
+app.get('/rdresults', function(req, res){
+    // Function below breaks the URL up given
+    // a URI such as /results?params='paramValue'&moreParams
+    function getParameterByName(name, url) {
+        if (!url) {
+            url = req.url;
+        }
+        name = name.replace(/[\[\]]/g, "\\$&");
+        var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+        if (!results){ 
+            return null;
+        }
+        if (!results[2]){
+            return '';
+        } 
+        return decodeURIComponent(results[2].replace(/\+/g, " "));
+    }
+
+    var firstName = getParameterByName('first');
+    var lastName = getParameterByName('last');
+    var dcn = getParameterByName('dcn');
+
+    var queryString = `SELECT * FROM Residence where firstname = '${firstName}' AND lastname = '${lastName}' AND DCNumber = '${dcn}'`;
+
+    connection.queryRunner(queryString, function(err, results){
+        if(err){
+            return err;
+        }
+        res.render('results.ejs', {user: req.session.user, records: results});
+    });
+});
+
+// Render the results on an offense search submission
+app.get('/rdresults', function(req, res){
+    // Function below breaks the URL up given
+    // a URI such as /results?params='paramValue'&moreParams
+    function getParameterByName(name, url) {
+        if (!url) {
+            url = req.url;
+        }
+        name = name.replace(/[\[\]]/g, "\\$&");
+        var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+        if (!results){ 
+            return null;
+        }
+        if (!results[2]){
+            return '';
+        } 
+        return decodeURIComponent(results[2].replace(/\+/g, " "));
+    }
+
+    var firstName = getParameterByName('first');
+    var lastName = getParameterByName('last');
+    var dcn = getParameterByName('dcn');
+    var county = getParameterByName('county');
+
+    var queryString = `SELECT * from Offense where `
+    connection.queryRunner(queryString, function(err, results){
+        if(err){
+            return err;
+        }
+        res.render('results.ejs', {user: req.session.user, records: results});
+    });
 });
 
 // Start the server
